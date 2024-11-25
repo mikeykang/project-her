e7fe79526c09485537f360b1bda64a9c7e8cde0c
mikeykang <mikeykang@gmail.com>
2024-11-25 15:21:12 +0900

    Initial Commit

---
+++ os1/os1App.swift
@@ -0,0 +1,17 @@
+//
+//  os1App.swift
+//  os1
+//
+//  Created by Seenyoung Kang on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct os1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
